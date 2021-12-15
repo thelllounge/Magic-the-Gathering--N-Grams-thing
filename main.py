f5bf28f2871d85fb2cb5f1e47f542707a63cb573
Dakota Shyft <46868624+thelllounge@users.noreply.github.com>
2021-12-15 06:56:36 +0000

    just narrowed down the print

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import json
 print('hello')
 cards = open('oracle-cards-20211214100357.json')
 card_data = json.load(cards)
-print(card_data[0])
+print(card_data[0]['name'], card_data[0]['oracle_text'])
 print('seriously?')
