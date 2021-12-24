debe899547dba41d9401ae6217a86e69fc5af187
thelllounge <llearninglounge@gmail.com>
2021-12-24 23:42:52 +0900

    just experimented more basically

--- main.py
+++ main.py
@@ -11,23 +11,34 @@ card_data = json.load(f)
 by_names = {}
 names = []
 oracle_text = []
+cmc = []
 
 # This is the loop that takes all of the cards and puts them in the by_names dictionary
-for card in card_data:
-  for name in card:
-    by_names[card['name']] = card
+#I don't think I need this... but I'm leaving it for now. I think the other loop does all of this better by itself.
+# for card in card_data:
+#   for name in card:
+#     by_names[card['name']] = card
 
-print(by_names['Static Orb']['oracle_text'])
-
+print(card_data[0]['cmc'])
 
 #I got the names to work, but the oracle text still isn't working for some reason.
+#I have tried color identity, CMC, type_line and they all work, but not oracle_text!
 for card in card_data:
   names.append(card['name'])
+  cmc.append(card['oracle_text'])
   # oracle_text.append(card['oracle_text'])
 
+# for card in card_data:
+#   for i in range(len(names)):
+#     oracle_text.append(card[i]['oracle_text'])
+
+print(type(card_data[0]['name']))
+print(type(card_data[0]['type_line']))
+print(type(card_data[0]['oracle_text']))
 
 print('top')
 print(names[0], names[100], names[1000])
 print('middle')
-# print(oracle_text[0], oracle_text[100], oracle_text[1000])
+print(len(names))
+print(cmc[0], cmc[100], cmc[1000])
 print('end')
